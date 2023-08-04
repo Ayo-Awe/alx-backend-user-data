f88183951c159ae9474de4499c1385372d87d754
Awe Ayomidipupo <pupoawe@gmail.com>
2023-08-04 22:10:37 +0100

    feat: solve task 3

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,6 +7,8 @@ alx personal data task in the alx backend specialisation
 from typing import List
 import logging
 import re
+import os
+import mysql.connector
 
 PII_FIELDS = ("name", "email", "phone", "password", "ssn")
 
@@ -52,3 +54,17 @@ def get_logger() -> logging.Logger:
     logger.addHandler(stream_handler)
 
     return logger
+
+
+def get_db() -> None:
+    """Returns a connector to a MySQL database
+    """
+    username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = os.getenv("PERSONAL_DATA_DB_NAME")
+    print(username, password, host, db_name)
+
+    return mysql.connector.connect(user=username, password=password,
+                                   host=host,
+                                   database=db_name)
