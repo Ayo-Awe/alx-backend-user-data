943a7b16fbded809a1855f930473ef16b5fb6bd8
Awe Ayomidipupo <pupoawe@gmail.com>
2023-08-04 15:02:33 +0100

    debug

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -9,31 +9,30 @@ from typing import List, Tuple
 import re
 
 
-# def filter_datum(fields: List[str], redaction: str,
-#                  message: str, separator: str) -> str:
-#     """Uses a regex to replace occurrences of certain field values"""
-#     pattern = r"({}=)([^{}]*)({})".format(
-#         "(?:{})".format("|".join(fields)), separator, separator)
-#     return re.sub(pattern, r"\1{}\3".format(redaction), message)
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """Uses a regex to replace occurrences of certain field values"""
+    pattern = r"({}=)([^{}]*)({})".format(
+        "(?:{})".format("|".join(fields)), separator, separator)
+    return re.sub(pattern, r"\1{}\3".format(redaction), message)
 
 
-# # class RedactingFormatter(logging.Formatter):
-# #     """ Redacting Formatter class
-# #         """
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
 
-# #     REDACTION: str = "***"
-# #     FORMAT: str = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-# #     SEPARATOR: str = ";"
+    REDACTION: str = "***"
+    FORMAT: str = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
 
-# #     def __init__(self, **kwargs: any) -> None:
-# #         """Constructor"""
-# #         super(RedactingFormatter, self).__init__(self.FORMAT)
+    def __init__(self, fields: List[str]) -> None:
+        """Constructor"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
 
-# #         if len(kwargs) > 0 and "fields" in kwargs:
-# #             self.__fields: Tuple[str] = kwargs["fields"]
+        self.__fields = fields
 
-# #     def format(self, record: logging.LogRecord) -> str:
-# #         """Formats the redacted logs"""
-# #         record.msg: str = filter_datum(
-# #             list(self.__fields), self.REDACTION, "; ".join(record.msg.split(";")), self.SEPARATOR)
-# #         return super().format(record)
+    def format(self, record: logging.LogRecord) -> str:
+        """Formats the redacted logs"""
+        record.msg: str = filter_datum(
+            list(self.__fields), self.REDACTION, "; ".join(record.msg.split(";")), self.SEPARATOR)
+        return super().format(record)
