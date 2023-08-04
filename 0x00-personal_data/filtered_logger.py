d1db7a064b5593735a2b80ad53727295459ae4d0
Awe Ayomidipupo <pupoawe@gmail.com>
2023-08-04 14:31:36 +0100

    feat: solve task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,7 +4,8 @@
 alx personal data task in the alx backend specialisation
 """
 
-from typing import List
+import logging
+from typing import List, Tuple
 import re
 
 
@@ -14,3 +15,23 @@ def filter_datum(fields: List[str], redaction: str,
     pattern = r"({}=)([^{}]*)({})".format(
         "(?:{})".format("|".join(fields)), separator, separator)
     return re.sub(pattern, r"\1{}\3".format(redaction), message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: Tuple[str]):
+        """Constructor"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.__fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Formats the redacted logs"""
+        record.msg = filter_datum(
+            list(self.__fields), self.REDACTION, "; ".join(record.msg.split(";")), self.SEPARATOR)
+        return super().format(record)
