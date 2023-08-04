3a8fa3354b3099358bc2da942d3a045ed7f873aa
Awe Ayomidipupo <pupoawe@gmail.com>
2023-08-04 11:38:10 +0100

    fix: resolve errors

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,9 +6,7 @@ import re
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
-    """ The function uses a regex to replace
-    occurrences of certain field values"""
-    res = re.compile(r"({}=)([^{}]*)({})".format(
-        "(?:{})".format("|".join(fields)), separator, separator))
-    b = res.sub(r"\1{}\3".format(redaction), message)
-    return b
+    """Uses a regex to replace occurrences of certain field values"""
+    pattern = r"({}=)([^{}]*)({})".format(
+        "(?:{})".format("|".join(fields)), separator, separator)
+    return re.sub(pattern, r"\1{}\3".format(redaction), message)
