1329e35f38c154ee1e7787bdade12e4d8e9f11c8
Awe Ayomidipupo <pupoawe@gmail.com>
2023-08-04 16:03:21 +0100

    fix

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -27,11 +27,9 @@ class RedactingFormatter(logging.Formatter):
     def __init__(self, fields: List[str]) -> None:
         """Constructor"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
-
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
         """Formats the redacted logs"""
         record.msg = filter_datum(self.fields, self.REDACTION,
                                   record.getMessage(), self.SEPARATOR)
-        return super().format(record)
