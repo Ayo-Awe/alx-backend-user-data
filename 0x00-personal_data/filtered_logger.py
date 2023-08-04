ea8d32db97a897bf829fc1edde76f983f6549349
Awe Ayomidipupo <pupoawe@gmail.com>
2023-08-04 20:20:24 +0100

    fxix

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -12,9 +12,10 @@ import re
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """Uses a regex to replace occurrences of certain field values"""
-    pattern = r"({}=)([^{}]*)({})".format(
-        "(?:{})".format("|".join(fields)), separator, separator)
-    return re.sub(pattern, r"\1{}\3".format(redaction), message)
+    for field in fields:
+        message = re.sub(field+'=.*?'+separator,
+                         field+'='+redaction+separator, message)
+    return message
 
 
 class RedactingFormatter(logging.Formatter):
