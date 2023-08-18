1316e2e6e728d4ced065b53e784b1cf6a5df1460
Awe Ayomidipupo <pupoawe@gmail.com>
2023-08-18 01:37:56 +0100

    feat(0x03): solve task 8

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -35,3 +35,13 @@ class Auth:
             hashed_password = _hash_password(password)
             user = self._db.add_user(email, hashed_password)
             return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Validates a user's credentials"""
+
+        # find user by email
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode(), user.hashed_password)
+        except NoResultFound:
+            return False
