a770110560953ea02eadbd8a36a0279e4715361f
Awe Ayomidipupo <pupoawe@gmail.com>
2023-08-18 01:41:30 +0100

    feat(0x03): solve task 9

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -8,6 +8,7 @@ from db import DB
 import bcrypt
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+import uuid
 
 
 def _hash_password(password: str) -> bytes:
@@ -16,6 +17,14 @@ def _hash_password(password: str) -> bytes:
     """
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(password.encode(), salt)
+
+
+def _generate_uuid() -> str:
+    """Generates and returns a new
+    uuid string
+    """
+
+    return str(uuid.uuid4())
 
 
 class Auth:
