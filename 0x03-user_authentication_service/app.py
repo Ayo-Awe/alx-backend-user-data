11d1b52ed5f3b9b7bee6ecb87aeaaf1ae88128c3
Awe Ayomidipupo <pupoawe@gmail.com>
2023-08-18 01:33:51 +0100

    feat(0x03): solve task 7

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -4,10 +4,12 @@
 the user auth service in python
 """
 from flask import Flask, jsonify, abort, request
+from auth import Auth
 # from flask_cors import (CORS, cross_origin)
 
 
 app = Flask(__name__)
+auth = Auth()
 # CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
@@ -16,6 +18,19 @@ def index():
     """index route handler"""
 
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def register_user():
+    """Registers a new user"""
+
+    body = request.form
+
+    try:
+        user = auth.register_user(body["email"], body["password"])
+        return jsonify({"email": user.email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
