64bcbfb7f145a6c1c883d989967189723da73866
Awe Ayomidipupo <pupoawe@gmail.com>
2023-08-18 02:01:38 +0100

    feat(0x03): solve task 11

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,7 +3,7 @@
 """This module contains code for
 the user auth service in python
 """
-from flask import Flask, jsonify, abort, request
+from flask import Flask, jsonify, abort, request, make_response
 from auth import Auth
 # from flask_cors import (CORS, cross_origin)
 
@@ -31,6 +31,23 @@ def register_user():
         return jsonify({"email": user.email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login():
+    """Login an existing user"""
+
+    body = request.form
+    is_credentials_valid = auth.valid_login(body["email"], body["password"])
+
+    if not is_credentials_valid:
+        abort(401)
+
+    session_id = auth.create_session(body["email"])
+    response = jsonify({"email": body["email"], "message": "logged in"})
+    response.set_cookie("session_id", session_id)
+
+    return response
 
 
 if __name__ == "__main__":
